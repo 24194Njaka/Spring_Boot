5c40ffdf119af238f4f2cbb980872b3448f6f04a
Njaka <hei.herinjaka@gmail.com>
2026-03-11 12:00:50 +0300

    feat: add BookingService

---
+++ src/main/java/com/hotel/booking/service/BookingService.java
@@ -0,0 +1,14 @@
+package com.hotel.booking.service;
+
+import com.hotel.booking.model.Booking;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookingService {
+
+    private final List<Booking> bookings = new ArrayList<>();
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }.
+}
